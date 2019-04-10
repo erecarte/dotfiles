242b86eeb0b9301728df9b52c29c8a099d7753b2
Enrique Recarte <recarte.enrique@gmail.com>
2019-04-10 10:02:36 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -17,6 +17,7 @@ fi
 
 brew install node
 brew install terraform
+brew install terraform_landscape
 brew install tree
 brew install wget --with-iri
 brew install graphviz
@@ -24,11 +25,17 @@ brew install plantuml
 brew install go
 brew install jq
 brew install awscli
+brew install git-secrets
+brew install gnutls
+brew install yubico-piv-tool
+brew install dep
+brew install go-swagger
+brew install travis
+brew install vault
+brew install hub
 brew install fish
-
-# Install Oh My Fish and bobthefish
-curl -L https://get.oh-my.fish | fish
-omf install bobthefish
+brew install bash-completion
+brew install avg-antivirus
 
 # Install Cask
 brew tap caskroom/cask
@@ -37,7 +44,6 @@ brew cask install java
 brew cask install java8
 brew cask install atom
 brew cask install google-chrome
-brew cask install caskroom/versions/google-chrome-canary
 brew cask install intellij-idea
 brew cask install spotify
 brew cask install slack
@@ -46,21 +52,21 @@ brew cask install docker
 brew cask install spectacle
 brew cask install iterm2
 brew cask install clipy
-brew cask install sequel-pro
 brew cask install postman
 brew cask install gpg-suite
+brew cask install zoomus
+brew cask install caskroom/fonts/font-firacode-nerd-font
+brew cask install goland
+brew cask install 1password
+
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
+
 npm install -g grunt-cli
 brew install maven
 brew install gradle
-
-apm install go-plus
-apm install hyperclick
-apm install linter-golinter
-apm install platformio-ide-terminal
 
 # Remove outdated versions from the cellar.
 brew cleanup
